d44f57f0829a75febaf018af82a8fcd24009fad4
Tan Beacri <beacri@o2.pl>
2017-02-28 15:40:50 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+<link href="style.css" rel="stylesheet">
+<script src="jquery-3.1.1.min.js"></script>
+<script src="gallery.js"></script>
+</head>
+<body>
+<div id='gallery'></div>
+
+</body>
+</html>
