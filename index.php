7bfba563b394dfe3a15c50a8598da3af53b783e8
Tan Beacri <beacri@o2.pl>
2017-02-28 16:22:57 +0100

    1st
    test of commit

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <!doctype html>
 <html>
 <head>
+	<title>Ezgal - Easy Gallery Demo</title>
 	<meta charset="utf-8">
-<link href="style.css" rel="stylesheet">
-<script src="jquery-3.1.1.min.js"></script>
-<script src="gallery.js"></script>
+	<link href="style.css" rel="stylesheet">
+	<script src="jquery-3.1.1.min.js"></script>
+	<script src="gallery.js"></script>
 </head>
 <body>
-<div id='gallery'></div>
+	<h1>Ezgal - Easy Gallery Demo</h1>
+	<div id='gallery'></div>
 
+	To jest przykładowy opis
 </body>
 </html>
