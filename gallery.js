077e2a6bc2c0cb2fbe3cc3a24506cfe1ec2ae1f0
Tan Beacri <beacri@o2.pl>
2017-03-13 18:45:08 +0100

    +
    1 bug fixed and 53 new unexpected features added

--- gallery.js
+++ gallery.js
@@ -10,6 +10,17 @@ $('.gallery-category').each(function(a,b){
 return $.ezgal.coords;
 }
 
+//stolen from https://stackoverflow.com/questions/210717/using-jquery-to-center-a-div-on-the-screen
+jQuery.fn.center = function () {
+    this.css("position","absolute");
+    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + 
+                                                $(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
+                                                $(window).scrollLeft()) + "px");
+    return this;
+}
+
+
 function EZgal_init(id, options){
 	$.ezgal.options = options;
 	var contrainer = $(id);
@@ -31,7 +42,7 @@ function EZgal_init(id, options){
 		}
 		var id = $(this).attr('data-ezgal-id');
 		miniwinhere = $.ezgal.coords.length-1;// convert 1-based to 0-based
-			id++;
+		id++;
 		for (var i = id; i <= $.ezgal.coords.length-1; i++) {
 			if ($.ezgal.coords[i][1]>0) {
 				miniwinhere = i-1;
@@ -45,22 +56,46 @@ function EZgal_init(id, options){
 		$($.ezgal.options[id].thumbnail).each(function(k,v){
 			images_in_gallery +='<li class="ezgal-little" data-ezgal-little-id="'+k+'"><img src="'+v+'"></li>';
 		});
-		$('[data-ezgal-id='+miniwinhere+']').after('<div id="ezgal-cat-inner" class="ezgal-hidden"><div id="ezgal-inner-left"><img src="'+$.ezgal.options[id].large[0]+'"></div><div id="ezgal-inner-right"><h2 class="ezgal">'+$.ezgal.options[id].title+'</h2><div>'+$.ezgal.options[id].description+'</div><div class="ezgal-little-slides"><div class="ezgal-wrapper"><ul class="egzal-incatgallery">'+images_in_gallery+'</ul></div></div></div></div>');
+		$('[data-ezgal-id='+miniwinhere+']').after('<div id="ezgal-cat-inner" class="ezgal-hidden"><div id="ezgal-inner-left"><a href="'+$.ezgal.options[id].large[0]+'" class="ezgal-preview"><img id="ezgal-big-img" src="'+$.ezgal.options[id].large[0]+'"></a></div><div id="ezgal-inner-right"><h2 class="ezgal">'+$.ezgal.options[id].title+'</h2><div>'+$.ezgal.options[id].description+'</div><div class="ezgal-little-slides"><div class="ezgal-wrapper"><ul class="egzal-incatgallery">'+images_in_gallery+'</ul></div></div></div></div>');
 		$('#ezgal-cat-inner').fadeIn(1000);
 
-		$('#ezgal-inner-left, #ezgal-inner-right').height(Math.max(Math.ceil($('#ezgal-inner-left').height()), Math.ceil($('#ezgal-inner-right').height())));
+		$('#ezgal-big-img').on('load', function(){
+			$('#ezgal-inner-left, #ezgal-inner-right').animate({height:Math.max(Math.ceil($('#ezgal-inner-left').height()), Math.ceil($('#ezgal-inner-right').height()))}, 1000);
+		});
+
 
 		//add bigger preview handlers
 		$('.ezgal-little').on('click', function(){
 			var id = $(this).attr('data-ezgal-little-id');
-			$('#ezgal-inner-left').html('<img src="'+$.ezgal.options[id].large[0]+'"></div>');
+			$('#ezgal-inner-left').html('<a href="'+$.ezgal.options[id].large[0]+'" class="ezgal-preview"><img id="ezgal-big-img" src="'+$.ezgal.options[id].large[0]+'"></a></div>');
 
+		$('#ezgal-big-img').on('load', function(){//repeadted code (engeeniering way, like tape, nails or WD40)
+			$('#ezgal-inner-left, #ezgal-inner-right').css('height', 'inherit');
+			$('#ezgal-inner-left, #ezgal-inner-right').animate({height:Math.max(Math.ceil($('#ezgal-inner-left').height()), Math.ceil($('#ezgal-inner-right').height()))}, 1000);
+		});
 		});
 	});
 
 }
+function preview(){
+	$('body').addClass('noscroll').append('<div class="ezgal-overlay"></div>').append('<div id="ezgal-preview-insider"><img src="img/big1.jpg"></div>');
+	$('#container').addClass('noscroll');
+	$('#ezgal-preview-insider img').center();
+	$('.ezgal-overlay').on('click', function(){close_preview();});
+	$('body').on('keydown', function(e){
+		if (e.keyCode === 27){
+			close_preview();
+		}
+		
+	});
+}
 
-
+function close_preview(){
+	$('body').removeClass('noscroll');
+	$('#container').removeClass('noscroll');
+	$('.ezgal-overlay,#ezgal-preview-insider').remove();
+	$('body').off('keydown');
+}
 $(document).ready(function(){
 
 	var options = 
